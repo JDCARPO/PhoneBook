d1fec491e74801c6cba21f0d874573467f52f7c1
JDCARPO <36685039+JDCARPO@users.noreply.github.com>
2019-01-10 09:29:42 +0800

    Update view.php

--- view.php
+++ view.php
@@ -39,7 +39,7 @@ $result = mysqli_query($db, "SELECT * FROM contacts WHERE login_id=".$_SESSION['
 							<div class="collapse navbar-collapse" id="navbarSupportedContent">
 								<ul class="navbar-nav mr-auto">
 									<li class="nav-item active">
-										<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+										<a class="nav-link" href="view.php">Phonebook <span class="sr-only">(current)</span></a>
 									</li>
 								</ul>
 								<form class="form-inline my-2 my-lg-0">		
