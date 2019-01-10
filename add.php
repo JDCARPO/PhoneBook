59769fb051932b78ef2cbdcfd39e6bb69ffa08d0
JDCARPO <36685039+JDCARPO@users.noreply.github.com>
2019-01-10 09:28:35 +0800

    Update add.php

--- add.php
+++ add.php
@@ -33,7 +33,7 @@ if(!isset($_SESSION['valid'])) {
 							<div class="collapse navbar-collapse" id="navbarSupportedContent">
 								<ul class="navbar-nav mr-auto">
 									<li class="nav-item active">
-										<a class="nav-link" href="register.php">Phonebook <span class="sr-only">(current)</span></a>
+										<a class="nav-link" href="view.php">Phonebook <span class="sr-only">(current)</span></a>
 									</li>
 								</ul>
 								<form class="form-inline my-2 my-lg-0">
